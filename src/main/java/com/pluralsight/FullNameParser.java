d33c2a30c760ecae6c050f17f034dac5347d2219
lijas <lijason991@gmail.com>
2025-09-29 21:57:57 -0400

    added comments

--- src/main/java/com/pluralsight/FullNameParser.java
+++ src/main/java/com/pluralsight/FullNameParser.java
@@ -6,17 +6,22 @@ public class FullNameParser {
 
     public static void main(String[] args) {
 
+        // names new scanner
         Scanner input = new Scanner(System.in);
 
+        // prompts user for full name
         System.out.println("Please enter your full first, middle, and last name: ");
         String fullName = input.nextLine();
 
+        // calls method fullNameParser to separate the names
         String[] parsedNames = fullNameParser(fullName);
 
+        // names each part of the outputted string list
         String first = parsedNames[0];
         String middle = parsedNames[1];
         String last = parsedNames[2];
 
+        // prints names out as formatted in assignment
         System.out.printf("""
                         First name: %s
                         Middle name: %s
@@ -26,15 +31,19 @@ public class FullNameParser {
 
     }
 
+    // method fullNameParser
     public static String[] fullNameParser(String fullName) {
 
+        // trims fullName, then separates them with split()
         fullName = fullName.trim();
         String[] separatedNames = fullName.split(" ");
 
+        // creates empty strings
         String first = "";
         String middle = "";
         String last = "";
 
+        // assigns split names to empty strings depending on how many there are
         switch (separatedNames.length) {
             case 1:
                 first = separatedNames[0];
@@ -55,11 +64,13 @@ public class FullNameParser {
                 System.out.println("You have not entered anything meaningful. Please restart.");
         }
 
+        // cleans up the names, capitalizing first letters and lowercasing others
         first = first.toUpperCase().charAt(0) + first.substring(1);
         middle = middle.toUpperCase().charAt(0) + middle.substring(1);
         ;
         last = last.toUpperCase().charAt(0) + last.substring(1);
 
+        // returns new list of parsed names
         return new String[]{first, middle, last};
 
     }
