a37cdb92252f225eae6598c85935522ddfe346f1
lijas <lijason991@gmail.com>
2025-10-01 00:54:13 -0400

    condensed code and fixed middle name error

--- src/main/java/com/pluralsight/FullNameGenerator.java
+++ src/main/java/com/pluralsight/FullNameGenerator.java
@@ -9,17 +9,17 @@ public class FullNameGenerator {
         // names scanner
         Scanner input = new Scanner(System.in);
 
-        // prompts user for all parts of name separately
+        // prompts user for all parts of name separately, trimming as they are entered
         System.out.println("Please enter parts of your name in the following prompts: ");
 
         System.out.println("First name: ");
-        String first = input.nextLine();
+        String first = input.nextLine().trim();
         System.out.println("Middle name: ");
-        String middle = input.nextLine();
+        String middle = input.nextLine().trim();
         System.out.println("Last name: ");
-        String last = input.nextLine();
+        String last = input.nextLine().trim();
         System.out.println("Suffix: ");
-        String suffix = input.nextLine();
+        String suffix = input.nextLine().trim();
 
         // calls method fullNameGenerator with all the inputs as arguments
         String fullName = fullNameGenerator(first, middle, last, suffix);
@@ -31,20 +31,20 @@ public class FullNameGenerator {
 
     public static String fullNameGenerator(String first, String middle, String last, String suffix) {
 
-        // first trim all the entered strings
-        first = first.trim();
-        middle = middle.trim();
-        last = last.trim();
-        suffix = suffix.trim();
-
         // for first and last names, capitalize the first and lowercase the other letters
-        // for middle names, capitalize the first letter and add a period
         first = first.toUpperCase().charAt(0) + first.substring(1).toLowerCase();
-        middle = middle.toUpperCase().charAt(0) + ".";
         last = last.toUpperCase().charAt(0) + last.substring(1).toLowerCase();
 
-        // create a string fullName made up of the first, middle, and last names
-        String fullName = String.join(" ", first, middle, last);
+        // create a string fullName made up of the first and last names
+        String fullName = String.join(" ", first, last);
+
+        // for middle names, capitalize the first letter and add a period only if it is not empty
+        // then add it to fullName after the first name by finding the index of the space
+        if (!middle.isEmpty()) {
+            middle = middle.toUpperCase().charAt(0) + ".";
+            fullName = fullName.substring(0, fullName.indexOf(" ")) + " " + middle +
+                    fullName.substring(fullName.indexOf(" ") + 1);
+        }
 
         // only add a suffix if it exists
         if (!suffix.isEmpty()) {
